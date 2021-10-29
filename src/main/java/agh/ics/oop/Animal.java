e8df774313f7e40c49088a14bedba5f5dd4abd9f
Mikołaj Wielgos <43356643+wlgs@users.noreply.github.com>
2021-10-29 13:56:53 +0200

    Aesthetic changes, better looking switch and so on

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -33,26 +33,17 @@ public class Animal {
             case BACKWARD:
                 opposite = true;
             case FORWARD:
-                Vector2d movementChange = new Vector2d(0, 0);
-                switch (this.direction) {
-                    case EAST:
-                        movementChange = movementChange.add(new Vector2d(1, 0));
-                        break;
-                    case WEST:
-                        movementChange = movementChange.add(new Vector2d(-1, 0));
-                        break;
-                    case NORTH:
-                        movementChange = movementChange.add(new Vector2d(0, 1));
-                        break;
-                    case SOUTH:
-                        movementChange = movementChange.add(new Vector2d(0, -1));
-                        break;
-                }
-                if (opposite) {
+                Vector2d movementChange = new Vector2d(0,0);
+                movementChange = switch (this.direction) {
+                    case EAST -> movementChange.add(new Vector2d(1, 0));
+                    case WEST -> movementChange.add(new Vector2d(-1, 0));
+                    case NORTH -> movementChange.add(new Vector2d(0, 1));
+                    case SOUTH -> movementChange.add(new Vector2d(0, -1));
+                };
+                if (opposite)
                     movementChange = movementChange.opposite();
-                }
                 Vector2d newPos = this.position.add(movementChange);
-                Vector2d mapBorderTR = new Vector2d(4, 4);
+                Vector2d mapBorderTR = new Vector2d(4, 4); // there should be a map class hopefully soon
                 Vector2d mapBorderBL = new Vector2d(0, 0);
                 if (newPos.precedes(mapBorderBL) && newPos.follows(mapBorderTR))
                     this.position = newPos;
