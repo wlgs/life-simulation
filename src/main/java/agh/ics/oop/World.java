fc2c6fceb3d538b3a519758f3dd8fa9065757ef9
Mikołaj Wielgos <43356643+wlgs@users.noreply.github.com>
2021-12-12 08:46:03 +0100

    world changes

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,37 +3,8 @@ package agh.ics.oop;
 import agh.ics.oop.gui.App;
 import javafx.application.Application;
 
-import java.util.Arrays;
-
 public class World {
     public static void main(String[] args) {
         Application.launch(App.class, args);
-    }
-
-    public static void run(Direction[] array) {
-        for (Direction d : array) {
-            switch (d) {
-                case FORWARD -> System.out.println("zwierzak idzie do przodu");
-                case BACKWARD -> System.out.println("zwierzak idzie do tylu");
-                case RIGHT -> System.out.println("zwierzak skręca w prawo");
-                case LEFT -> System.out.println("zwierzak skręca w lewo");
-                default -> {
-                }
-            }
-        }
-    }
-
-    public static Direction[] convert(String[] array) {
-        Direction[] result = new Direction[array.length];
-        for (int i = 0; i < array.length; i++) {
-            switch (array[i]) {
-                case "f" -> result[i] = Direction.FORWARD;
-                case "b" -> result[i] = Direction.BACKWARD;
-                case "r" -> result[i] = Direction.RIGHT;
-                case "l" -> result[i] = Direction.LEFT;
-                default -> result[i] = Direction.UNKNOWN;
-            }
-        }
-        return result;
     }
 }
